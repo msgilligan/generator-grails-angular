f662c4f4805623a0deabb8e15fcf8476f9ddae30
Zan Thrash <zanthrash@gmail.com>
2013-04-16 21:09:14 -0500

    Generated tests should be suffixed with 'Spec'

--- script-base.js
+++ script-base.js
@@ -75,7 +75,7 @@ Generator.prototype.appTemplate = function (src, dest) {
 Generator.prototype.testTemplate = function (src, dest) {
   yeoman.generators.Base.prototype.template.apply(this, [
     src + this.scriptSuffix,
-    path.join(this.env.options.testPath, dest) + this.scriptSuffix
+    path.join(this.env.options.testPath, dest) + 'Spec' + this.scriptSuffix
   ]);
 };
 
